4b6e3f80be9d9736455fc0a534518e23dab62fab
Michael Meyer <michael.meyer@improving.com>
2021-06-01 08:29:33 -0500

    Initial (and probably only) commit

---
+++ KnockKnock/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace KnockKnock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Knock knock!");
+
+            string resp = Console.ReadLine();
+            resp = resp.ToLower();
+
+            while (!resp.Contains("who's there") && !resp.Contains("whos there"))
+            {
+                Console.WriteLine("Knock knock!");
+                resp = Console.ReadLine();
+                resp = resp.ToLower();
+            }
+
+            Console.WriteLine("Mikey.");
+
+            string resp2 = Console.ReadLine();
+            resp2 = resp2.ToLower();
+
+            while (!resp2.Contains("mikey who"))
+            {
+                Console.WriteLine("Try again.");
+                resp2 = Console.ReadLine();
+                resp2 = resp2.ToLower();
+            }
+
+            Console.WriteLine("Mikey isn't working, can you let me in?");
+        }
+    }
+}
